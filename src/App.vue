00fb61919310169ab1ceaea7f258ecd8ea8bba86
genaduttt <ggenaduttt@gmail.com>
2025-01-22 15:32:34 +0700

    commit pertama

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <div id="app">
+    <NavBar /> <!-- Panggil komponen NavBar -->
+    <h1>Welcome to Vue.js with Bootstrap!</h1>
+    <!-- Tambahkan konten lainnya di sini -->
+  </div>
+</template>
+
+<style scoped>
+template {
+    font-family: Arial, sans-serif;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/popeye.png" alt="Logo" width="100" height="40" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="#">Shop</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">Promo</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">Article</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">About</a></li>
+                </ul>
+                <button class="btn btn-outline-primary ms-auto">Sign up</button>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
